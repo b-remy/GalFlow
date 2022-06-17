d5d8f095c2e5f822a374eea8217b98dfa6cca6b7
Benjamin Remy <remy.benjamin.101@gmail.com>
2022-06-17 13:52:11 +0200

    fix init file

--- galflow/__init__.py
+++ galflow/__init__.py
@@ -1,4 +1,3 @@
 """Tools for simulating galaxy images in TensorFlow."""
 
-#from galflow.python import *  # pylint: disable=wildcard-import
-from galflow.python.shear import shear
+from galflow.python import *  # pylint: disable=wildcard-import
